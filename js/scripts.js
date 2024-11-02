50221ca702b1a4ddc0dde3f7448b21b49ac63ffb
MatheusDevPro <factorloock123@gmail.com>
2024-11-02 19:31:10 -0300

    js adicionado

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-// aquisição do código javascript restante faltante para //inserção de evento no gerador
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Após refatoração
+const openCloseGeneratorButton = document.querySelector(
+  "#open-generate-password"
+);
+const generatePasswordContainer = document.querySelector("#generate-options");
+const lengthInput = document.querySelector("#length");
+const lettersInput = document.querySelector("#letters");
+const numbersInput = document.querySelector("#numbers");
+const symbolsInput = document.querySelector("#symbols");
+const copyPasswordButton = document.querySelector("#copy-password");
+
+// Funções
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+  
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+  
+const getNumber = () => {
+    return Math.floor(Math.random() * 11).toString();
+};
+  
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%^&*";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
